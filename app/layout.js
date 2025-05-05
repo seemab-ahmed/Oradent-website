132a906352b68ce4988447f81cdb0cde14a18c62
Seemab Ahmed <seemab40615@gmail.com>
2025-05-05 21:22:16 +0500

    Initial Repo

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/component/Header";
+import Footer from "@/component/footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Skills 2 Evolve",
+  description: "Skills 2 Evolve",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
